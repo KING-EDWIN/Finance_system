3e387d106cccb30e411ad1cd820e38d12d6679e9
KING-EDWIN <damanifesta0@gmail.com>
2023-08-28 11:56:56 -0700

    saved changes  for settings file, wsgi file, keeping html, and vercel file

--- clari6/settings.py
+++ clari6/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-lkvze+!l#&7!#kdwyai70w+_vxba&hn)l4)18b&4a^o$8b6u93
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
